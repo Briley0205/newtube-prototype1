04b03fc647acc8c0fb5ae7f27ed6fa30187152c2
unknown <81542046+Briley0205@users.noreply.github.com>
2022-02-15 14:46:14 +0900

    listening

--- src/newserver.js
+++ src/newserver.js
@@ -1,5 +1,19 @@
 import express from "express";
 
+const PORT = 4000;
+
 const app = express();
 
-console.log("I feel so dezy");
+const handleHome = (req, res) => {
+    return res.send("I'm still watching you.");
+}
+
+const handleLogin = (req, res) => {
+    return res.send("login here.")
+}
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
+const handleServerListening = () => console.log("Server is listening. I'm ready");
+
+app.listen(PORT, handleServerListening);
