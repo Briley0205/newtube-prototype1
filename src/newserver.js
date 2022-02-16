7a32d2a6572b7cc64cb73c5875119af4950c7003
unknown <81542046+Briley0205@users.noreply.github.com>
2022-02-16 13:58:20 +0900

    middlewares

--- src/newserver.js
+++ src/newserver.js
@@ -4,13 +4,20 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = (req, res) => {
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+}
+
+const handleHome = (req, res, next) => {
     return res.send("<h1>I'm still watching you.</h1>");
 }
 
 const handleLogin = (req, res) => {
     return res.send("login here.")
 }
+
+app.use(logger);
 app.get("/", handleHome);
 app.get("/login", handleLogin);
 
