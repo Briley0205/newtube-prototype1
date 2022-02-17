0ae32a06004c0110553f774e45a4b5cab8f338d4
unknown <81542046+Briley0205@users.noreply.github.com>
2022-02-17 21:59:15 +0900

    dizet

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -5,7 +5,7 @@ const userRouter = express.Router();
 
 userRouter.get("/edit", edit);
 userRouter.get("/delete", getOut);
-userRouter.get(":id", see);
-userRouter.get("logout", logout);
+userRouter.get("/logout", logout);
+userRouter.get("/:id(\\d+)", see);
 
 export default userRouter;
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,11 +3,10 @@ import {Watch, Edit, deleteVideo, upload} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id", Watch);
-videoRouter.get("/:id/edit", Edit);
-videoRouter.get("/:id/edit", Edit);
-videoRouter.get("/:id/delete", deleteVideo);
-videoRouter.get("/:id/upload", upload);
+videoRouter.get("/upload", upload);
+videoRouter.get("/:id(\\d+)", Watch);
+videoRouter.get("/:id(\\d+)/edit", Edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
 
 export default videoRouter;
