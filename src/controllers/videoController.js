3f6f07488cc4180a8cee9c9423fa65146d7571fe
unknown <81542046+Briley0205@users.noreply.github.com>
2022-02-18 10:32:34 +0900

    pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,6 @@
-export const trending = (req, res) => res.send("Home page videos");
-export const Watch = (req, res) => res.send("Watch Video");
-export const Edit = (req, res) => res.send("You can edit your video here");
+export const trending = (req, res) => res.render("home");
+export const Watch = (req, res) => res.render("watch");
+export const Edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("You can search your videos");
 export const upload = (req, res) => res.send("You can upload your videos here");
 export const deleteVideo = (req, res) => res.send("You may delete your videos");
