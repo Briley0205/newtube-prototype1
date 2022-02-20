e6e8fc21f265bf61a7ba2c9b0524064fff7774b9
unknown <81542046+Briley0205@users.noreply.github.com>
2022-02-20 16:30:45 +0900

    mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,30 @@
-export const trending = (req, res) => res.render("home", {pageTitle: "Home"});
+const fakeUser = {
+    username:"Boyeon",
+    loggedIn: false,
+}
+
+export const trending = (req, res) => {
+    const videos = [
+        {title: "I",
+         rating: 4.8,
+         comments: 378,
+         createdAt: "2 minutes ago",
+         views: 78,
+         id: 1,},
+         {title: "hate",
+         rating: 4.8,
+         comments: 378,
+         createdAt: "2 minutes ago",
+         views: 78,
+         id: 1,},
+         {title: "medicines",
+         rating: 4.8,
+         comments: 378,
+         createdAt: "2 minutes ago",
+         views: 78,
+         id: 1,},
+    ];
+    return res.render("home", {pageTitle: "Home", fakeUser, videos})};
 export const Watch = (req, res) => res.render("watch", {pageTitle: "watch"});
 export const Edit = (req, res) => res.render("edit", {pageTitle: "Edit videos"});
 export const search = (req, res) => res.send("You can search your videos");
