7725bb4fbb80f0178f31268bbf2e2b9b6cd1f76b
unknown <81542046+Briley0205@users.noreply.github.com>
2022-03-22 15:52:29 +0900

    google login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -181,6 +181,74 @@ export const finishKakaoLogin = async(req, res) => {
     }
 }
 
+export const startGoogleLogin = (req, res) => {
+    const baseUrl = "https://accounts.google.com/o/oauth2/v2/auth";
+    const config = {
+        client_id: process.env.GG_CLIENT,
+        redirect_uri: "http://localhost:4000/users/google/finish",
+        response_type: "code",
+        include_granted_scopes: true,
+        scope: ["https://www.googleapis.com/auth/userinfo.email",
+        "https://www.googleapis.com/auth/userinfo.profile"].join(" "),
+    }
+    const params = new URLSearchParams(config).toString();
+    const finalUrl = `${baseUrl}?${params}`;
+    return res.redirect(finalUrl);
+}
+export const finishGoogleLogin = async(req, res) => {
+    const baseUrl = "https://oauth2.googleapis.com/token";
+    const config = {
+        client_id: process.env.GG_CLIENT,
+        client_secret: process.env.GG_SECRET,
+        redirect_uri: "http://localhost:4000/users/google/finish",
+        include_granted_scopes: true,
+        grant_type: "authorization_code",
+        code: req.query.code
+    }
+    const params = new URLSearchParams(config).toString();
+    const finalUrl = `${baseUrl}?${params}`;
+    const tokenRequest = await(
+        await fetch(finalUrl, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/x-www-form-urlencoded",
+            }
+        })
+    ).json();
+    if("access_token" in tokenRequest){
+        console.log("I'm here!");
+        const { access_token } = tokenRequest;
+        console.log(access_token);
+        const apiUrl = "https://www.googleapis.com"
+        const userData = await (await fetch(`${apiUrl}/oauth2/v1/userinfo`, {
+            headers: {
+                Authorization: `Bearer ${access_token}`
+            }
+        })).json();
+        if(userData.verified_email === false) {
+            //set notification
+            res.redirect("/login");
+        }
+        let user = await User.findOne({ email: userData.email });
+        if(!user){
+            console.log("I'm reading here now...");
+            user = await User.create({
+                avatarUrl: userData.picture,
+                name: userData.name,
+                username: userData.name,
+                email: userData.email,
+                password: "",
+                socialOnly: true,
+            });
+        }
+        req.session.loggedIn = true;
+        req.session.user = user;
+        return res.redirect("/");
+    } else {
+        return res.redirect("/login");
+    }
+}
+
 export const edit = (req, res) => res.send("You can edit your profiles");
 export const getOut = (req, res) => res.send("You can delete your account.");
 export const logout = (req, res) => {
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { edit, getOut, see, logout, startGithubLogin, finishGithubLogin, startKakaoLogin, finishKakaoLogin } from "../controllers/userController";
+import { edit, getOut, see, logout, startGithubLogin, finishGithubLogin, startKakaoLogin, finishKakaoLogin, startGoogleLogin, finishGoogleLogin } from "../controllers/userController";
 
 const userRouter = express.Router();
 
@@ -11,6 +11,9 @@ userRouter.get("/github/finish", finishGithubLogin);
 
 userRouter.get("/kakao/start", startKakaoLogin);
 userRouter.get("/kakao/finish", finishKakaoLogin);
+
+userRouter.get("/google/start", startGoogleLogin);
+userRouter.get("/google/finish", finishGoogleLogin);
 userRouter.get("/:id(\\d+)", see);
 
 export default userRouter;
