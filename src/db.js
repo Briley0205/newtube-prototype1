02968aec7c15fbc211017a70d2bc3d79a62360fe
unknown <81542046+Briley0205@users.noreply.github.com>
2022-03-27 16:44:26 +0900

    ani data

--- src/db.js
+++ src/db.js
@@ -9,3 +9,17 @@ const handleOpen = () => console.log("Connected to DB");
 
 db.on("error", (error) => console.log("DB Error", error));
 db.once("open", handleOpen);
+
+//animation db
+import axios from "axios";
+import fetch from "node-fetch";
+import res from "express/lib/response";
+
+const anilist  = require('anilist-node');
+const Anilist = new anilist();
+/**Anilist.media.anime(132405).then(data => {
+    console.log(data);
+})
+Anilist.media.anime(21708).then(data => {
+    console.log(data);
+}); */
