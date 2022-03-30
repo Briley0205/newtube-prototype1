890c63e6486c110eb241f34a1956ae902ff1b9fe
unknown <81542046+Briley0205@users.noreply.github.com>
2022-03-30 14:59:17 +0900

    video play

---
+++ src/client/js/videoPlayer.js
@@ -0,0 +1,27 @@
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volumeRange = document.getElementById("volume");
+
+const handlePlay = (e) => {
+    if(video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+    playBtn.innerText = video.paused ? "Play" : "Pause";
+}
+
+const handleMute = (e) => {
+    if(video.muted) {
+        video.muted = false;
+    } else {
+        video.muted = true;
+    }
+    muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+    volumeRange.value = video.muted ? 0 : 0.5;
+}
+
+playBtn.addEventListener("click", handlePlay);
+muteBtn.addEventListener("click", handleMute);
